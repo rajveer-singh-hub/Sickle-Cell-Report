6c59863707c9bd1b9a3a0cc1e72f51f01cda554a
rajveer-singh-hub <rajveersinghveer1230@gmail.com>
2026-05-07 22:56:12 +0530

    Sickle Cell report

---
+++ service-worker.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = "cbc-cleaner-v2";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
+
+// Install
+self.addEventListener("install", (event) => {
+  self.skipWaiting(); // Force the waiting service worker to become the active service worker
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Activate
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+// Fetch
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
